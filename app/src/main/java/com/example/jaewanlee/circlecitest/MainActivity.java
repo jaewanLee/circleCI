b3d93324c83275c4848a67ababad2372457e75ff
leejaewan <ian_hodge@ab180.co>
2017-11-16 13:57:46 +0900

    test: fixed ignored branch

--- app/src/main/java/com/example/jaewanlee/circlecitest/MainActivity.java
+++ app/src/main/java/com/example/jaewanlee/circlecitest/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "ignore branch", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "ignored branch", Toast.LENGTH_SHORT).show();
 
     }
 }
